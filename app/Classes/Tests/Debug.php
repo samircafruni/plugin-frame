c9acf4c97bee6a9ba660413842ff78b5b6ccae66
Samir Cafruni <samircafruni@gmail.com>
2022-10-26 18:38:31 -0300

    Update Debug.php

---
+++ app/Classes/Tests/Debug.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Author - Plugin Name
+ *
+ * @package Arquivar-woocommerce-coupons/Classes/Abstracts
+ * @since   1.0.0
+ * @version 1.0.0
+ */
+
+namespace App\Classes\Core;
+
+/**
+ * Previne que o arquivo não seja acessado diretamente por uma URL
+ */
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class Debug
+{
+
+    public function __construct()
+    {
+        //
+    }
+}
